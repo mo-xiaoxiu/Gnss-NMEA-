edb911eeaa1071b81b452303eb917e6ee1ba1aab
mo-xiaoxiu <1807115463@qq.com>
2024-12-12 20:32:47 +0800

    [ADD]add nmea prase py

--- main.py
+++ main.py
@@ -3,6 +3,19 @@ import re
 from datetime import datetime
 import glob
 import pandas as pd  # 导入pandas库
+# import statelilteData_vis as sv
+import nmeaParse_single as nmeasingle
+from tkinter import *
+from tkinter import filedialog, messagebox
+import time
+from tkcalendar import Calendar  # 用于日期选择控件
+import pyautogui
+import numpy as np
+
+#打开输出文件以写入定位信息
+def format_time(timestamp):
+    """将时间戳格式化为 YYYY-MM-DD_HH-MM 格式"""
+    return datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d_%H-%M')
 
 def dms_to_decimal(degrees, direction):
     """将度分秒格式转换为十进制度格式"""
@@ -12,14 +25,25 @@ def dms_to_decimal(degrees, direction):
         decimal_degree = -decimal_degree
     return decimal_degree
 
-def parse_nmea_log(start_timestamp, end_timestamp):
-    # 获取当前目录下所有以nmea.log开头的文件
-    files = glob.glob('nmea.log*')  # 匹配以nmea.log开头的所有文件
+def parse_nmea_log(start_timestamp, end_timestamp, input_dir, output_dir, show_flag):
+    # 格式化开始时间和结束时间
+    start_time_str = format_time(start_timestamp)
+    end_time_str = format_time(end_timestamp)
 
-    # 打开输出文件以写入定位信息
-    location_file = 'location_data_2024-10-31_10-33.txt'
-    satellites_file = 'satellites_data_2024-10-31_10-33.xlsx'
-    bd_satellites_file = 'bd_satellites_data_2024-10-31_10-33.xlsx'
+    # 确保output文件夹存在
+    # output_dir = 'output'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)  # 如果output文件夹不存在，则创建它
+
+    # 获取当前目录下所有以nmea.log开头的文件
+    files = glob.glob(os.path.join(input_dir, 'nmea.log*'))  # 匹配以nmea.log开头的所有文件
+
+    # 构造输出文件路径
+    location_file = os.path.join(output_dir, f'location_data_{start_time_str}_{end_time_str}.txt')
+    location_excel = os.path.join(output_dir, f'location_data_{start_time_str}_{end_time_str}.xlsx')
+    satellites_file = os.path.join(output_dir, f'satellites_data_{start_time_str}_{end_time_str}.xlsx')
+    bd_satellites_file = os.path.join(output_dir, f'bd_satellites_data_{start_time_str}_{end_time_str}.xlsx')
+    error_location_excel = os.path.join(output_dir, f'error_location_data_{start_time_str}_{end_time_str}.xlsx')
 
     # 初始化计数器用于生成name
     count = 1
@@ -27,6 +51,7 @@ def parse_nmea_log(start_timestamp, end_timestamp):
     location_data = []
     satellites_data = []
     bd_satellites_data = []
+    error_location_data = []  # 用于存储经纬度为0的记录
 
     # 遍历所有相关的文件
     for file in files:
@@ -48,7 +73,7 @@ def parse_nmea_log(start_timestamp, end_timestamp):
                 # 如果时间戳在指定范围内，继续处理
                 if current_timestamp is not None and start_timestamp <= current_timestamp <= end_timestamp:
                     # 解析GNRMC或GNGNS语句
-                    if line.startswith('$GNRMC') or line.startswith('$GNGNS'):
+                    if line.startswith('$GNRMC'):
                         # 解析GNRMC或GNGNS，获取经纬度和高程信息
                         gnrmc_match = re.match(
                             r'^\$GNRMC,\d+\.\d+,[AV],([\d\.]+),([NS]),([\d\.]+),([EW]),.*?,.*?,.*?,.*?,.*?$', line)
@@ -62,11 +87,14 @@ def parse_nmea_log(start_timestamp, end_timestamp):
                             lon_decimal = dms_to_decimal(lon, lon_dir)
 
                         # 解析GNGNS语句，获取高程信息
-                        gngns_match = re.match(
-                            r'^\$GNGNS,\d+\.\d+,.+?,([\d\.]+),([NS]),([\d\.]+),([EW]),.*?,.*?,.*?,([\d\.]+),.*',
-                            line)
-                        if gngns_match:
-                            elevation = float(gngns_match.group(5))
+                        # gngns_match = re.match(
+                        #     r'^\$GNGNS,\d+\.\d+,.+?,([\d\.]+),([NS]),([\d\.]+),([EW]),.*?,.*?,.*?,([\d\.]+),.*',
+                        #     line)
+                        # if gngns_match:
+                        #     elevation = float(gngns_match.group(5))
+
+                        if lat_decimal == 0 or lon_decimal == 0:
+                            error_location_data.append([current_timestamp, lat_decimal, lon_decimal, elevation])
 
                         # 将位置信息保存到location_data
                         location_data.append([current_timestamp, lon_decimal, lat_decimal, elevation])
@@ -132,6 +160,12 @@ def parse_nmea_log(start_timestamp, end_timestamp):
         for count, (timestamp, lon, lat, elev) in enumerate(location_data, start=1):
             out.write(f"{lon},{lat},{elev},m{count}\n")
 
+    # 保存错误数据到Excel（经纬度为0的记录）
+    if error_location_data:
+        df_error_location = pd.DataFrame(error_location_data,
+                                         columns=['Timestamp', 'Longitude', 'Latitude', 'Elevation'])
+        df_error_location.to_excel(error_location_excel, index=False)
+
     # 保存卫星数据到Excel文件
     df_location = pd.DataFrame(location_data, columns=['Timestamp', 'Longitude', 'Latitude', 'Elevation'])
     df_location.to_excel('locationd_2024-10-31_10-33.xlsx', index=False)
@@ -144,11 +178,114 @@ def parse_nmea_log(start_timestamp, end_timestamp):
     df_bd_satellites = pd.DataFrame(bd_satellites_data, columns=['Timestamp', 'PRN', 'Elevation', 'Azimuth', 'SNR'])
     df_bd_satellites.to_excel(bd_satellites_file, index=False)
 
-    print(f"定位数据已保存至 {location_file}")
-    print(f"卫星数据已保存至 {satellites_file}")
+    # sv.stateliltes_data_vis(satellites_file, show_flag)
+
+
+# Tkinter GUI
+class NMEAParserApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title('NMEA Parser')
+        self.root.geometry('1000x1000')
+
+        self.input_dir = None
+        self.output_dir = None
+
+        # Tkinter GUI 日期时间选择
+        self.start_data_label = Label(root, text='选择nmea日志分析的开始时间(YYYY-MM-DD HH:MM:SS)')
+        self.start_data_label.pack(pady=5)
+        self.start_data_entry = Entry(root) # 允许手动输入
+        self.start_data_entry.pack(pady=5)
+
+        self.end_data_label = Label(root, text="选择nmea日志分析的结束时间(YYYY-MM-DD HH:MM:SS)")
+        self.end_data_label.pack(pady=5)
+        self.end_data_entry = Entry(root) # 允许手动输入
+        self.end_data_entry.pack(pady=5)
+
+        # 文件路径选择按钮
+        self.input_button = Button(root, text="选择nmea日志文件路径", command=self.select_input_path)
+        self.input_button.pack(pady=10)
+
+        self.output_button = Button(root, text="选择分析结果输出路径", command=self.select_output_path)
+        self.output_button.pack(pady=10)
+
+        self.output_button = Label(root, text="选择生成图表类型")
+        self.output_button.pack(pady=10)
+
+        self.chart_type_var = StringVar(value='scatter')
+        self.scatter_radio = Radiobutton(root, text="散点图", variable=self.chart_type_var, value='scatter')
+        self.scatter_radio.pack(pady=5)
+        self.line_radio = Radiobutton(root, text="折线图", variable=self.chart_type_var, value='line')
+        self.line_radio.pack(pady=5)
+        self.bar_radio = Radiobutton(root, text="柱状图", variable=self.chart_type_var, value='bar')
+        self.bar_radio.pack(pady=5)
+
+        self.generate_button = Button(root, text="分析数据", command=self.generate_chart)
+        self.generate_button.pack(pady=20)
+
+        # 输入NMEA报文的文本框和按钮
+        self.nmea_label = Label(root, text="输入nmea报文（每条以回车分隔）")
+        self.nmea_label.pack(pady=10)
+
+        self.nmea_input = Text(root, height=10, width=20)
+        self.nmea_input.pack(pady=10)
+
+        self.nmea_button = Button(root, text="解析nmea报文", command=self.parse_nmea_input)
+        self.nmea_button.pack(pady=10)
+
+    def show_succ(self, message):
+        succ_window = Toplevel(self.root)
+        succ_window.title("完成")
+        Label(succ_window, text=message, fg="blue").pack(pady=20)
+        Button(succ_window, text="确定", command=succ_window.destroy).pack(pady=5)
+
+    def show_error(self, message):
+        error_window = Toplevel(self.root)
+        error_window.title("错误")
+        Label(error_window, text=message, fg="red").pack(pady=20)
+        Button(error_window, text="关闭", command=error_window.destroy).pack(pady=5)
+
+    def select_input_path(self):
+        self.input_dir = filedialog.askdirectory(title="选择待nmea日志输入的文件路径")
+
+    def select_output_path(self):
+        self.output_dir = filedialog.askdirectory(title="选择分析结果输出的文件路径")
+
+    # 解析用户输入的nmea报文
+    def parse_nmea_input(self):
+        nmea_data = self.nmea_input.get("1.0", END).strip()
+        if not nmea_data:
+            messagebox.showerror("错误", "请输入nmea报文")
+            return
+
+        # 将每行nmea报文解析并显示在文本框内
+        nmea_lines = nmea_data.split('\n')
+
+        nmeasingle.prase_nmea(nmea_lines)
+
+        self.show_succ("解析完成！")
+
+    def generate_chart(self):
+        if self.input_dir and self.output_dir:
+            try:
+                start_time = int(datetime.strftime(self.start_data_entry.get(), "%Y-%m-%d %H:%M:%S").timestamp())
+                end_time = int(datetime.strftime(self.end_data_entry.get(), "%Y-%m-%d %H:%M:%S").timestamp())
+                if start_time < end_time:
+                    parse_nmea_log(start_time, end_time, self.input_dir, self.output_dir, self.chart_type_var.get())
+                else:
+                    self.show_error("请确保开始时间和结束时间正确！")
+            except ValueError:
+                self.show_error("无效的时间格式！请确保格式为 'YYYY-MM-DD HH:MM:SS'. ")
+                return
+        else:
+            self.show_error("请确保输入有效的输入/输出文件路径")
+
 
 # 调用函数
 if __name__ == "__main__":
-    start_time = 1730341980  # 开始时间戳  2024-11-11 11:10  2024-11-05 19:40 2024-11-05 19:47  2024-11-05 19:41:48 2024-11-11 11:40 2024-10-31 10:33
-    end_time = 1730342100  # 结束时间戳 2024-11-11 11:20  2024-11-05 20:00 2024-11-05 19:49  2024-11-05 19:48:43 2024-11-11 11:42 2024-10-31 10:35
-    parse_nmea_log(start_time, end_time)
+    # start_time = 1730341980  # 开始时间戳  2024-11-11 11:10  2024-11-05 19:40 2024-11-05 19:47  2024-11-05 19:41:48 2024-11-11 11:40 2024-10-31 10:33
+    # end_time = 1730342100  # 结束时间戳 2024-11-11 11:20  2024-11-05 20:00 2024-11-05 19:49  2024-11-05 19:48:43 2024-11-11 11:42 2024-10-31 10:35
+    # parse_nmea_log(start_time, end_time)
+    root = Tk()
+    app = NMEAParserApp(root)
+    root.mainloop()
---
+++ nmeaParse_single.py
@@ -0,0 +1,176 @@
+import time
+import datetime
+import pandas as pd
+
+# 将nmea时间转换为时间戳和北京时间
+def conver_time_to_timestamp_and_bj(time_str):
+    # nmea时间格式：HHMMSS.sss
+    utc_time = time_str[:6] # HHMMSS
+    fractional_seconds = float(time_str[6:]) # sss
+
+    # 转换度时分秒
+    hours = int(utc_time[:2])
+    minutes = int(utc_time[2:4])
+    seconds = int(utc_time[4:6]) # 只取整数部分作为标准秒数
+
+    # 获取utc时间戳
+    utc_time = datetime.datetime(2000, 1, 1, hours, minutes, seconds)
+
+    # 用timedelta调整浮动的小数秒部分
+    utc_time  += datetime.timedelta(seconds=fractional_seconds)
+
+    # 转换UNIX时间戳
+    timestamp = time.mktime(utc_time.timetuple())
+
+    # 北京时间为utc+8
+    bj_time = utc_time + datetime.timedelta(hours=8)
+
+    return timestamp, bj_time.strftime('%Y-%m-%d %H:%M:%S')
+
+
+# 转换将nmea经纬度
+def convert_to_decimal(degress_minutes_str):
+    # 经纬度格式：DDMM.MMMM
+    degrees = float(degress_minutes_str)
+    decimal_degrees = degrees // 100 + (degrees % 100) / 60.0
+    return decimal_degrees
+
+
+# 解析nmea报文
+def prase_nmea(sentence):
+    parsed_results = []
+    table_data = []
+
+    for line in sentence:
+        if not line.startswith('$'):
+            raise ValueError("Invalid NMEA sentence, it must start with $")
+
+        # 计算校验和
+        checksum = line.split('*')[-1]
+        data = line[1:].split('*')[0]  # 去除‘$’和校验和后面的部分
+
+        # 提取数据句子的类型
+        sentence_type = data.split(',')[0]
+
+        parsed_data = {'sentence_type': sentence_type, 'GGA': {}, 'RMC':{}, 'GNS':{}, 'GSV':{}}
+
+        # 初始化一个字典，用于存储行数据
+        row = {
+            'sentence_type': parsed_data.get('sentence_type'),
+            'time': None, "timestamp": None, 'beijing_time': None,
+            'latitude': None, 'longitude': None, 'fix_quality': None,
+            'statellites': None, 'altitude': None, 'status': None, 'speed': None, 'date': None
+        }
+
+        # 不同句子的解析方法
+        if sentence_type in ['GPGGA', 'BDGGA', 'GLGGA']:
+            parts = data.split(',')
+            timestamp, bj_time = conver_time_to_timestamp_and_bj(parts[1])
+            latitude = convert_to_decimal(parts[2]) if parts[3] == 'N' else -convert_to_decimal(parts[2])
+            longitude = convert_to_decimal(parts[4]) if parts[5] == 'E' else -convert_to_decimal(parts[4])
+
+            parsed_data['GGA'] = {
+                'time': parts[1],
+                'timestamp': timestamp,
+                'beijing_time': bj_time,
+                'latitude': latitude,
+                'longitude': longitude,
+                'fix_quality': parts[6],
+                'statellites': parts[7],
+                'altitude': parts[9]
+            }
+            row.update({
+                'time': parsed_data['GGA'].get('time'),
+                'timestamp': parsed_data['GGA'].get('timestamp'),
+                'beijing_time': parsed_data['GGA'].get('beijing_time'),
+                'latitude': parsed_data['GGA'].get('latitude'),
+                'longitude': parsed_data['GGA'].get('longitude'),
+                'fix_quality': parsed_data['GGA'].get('fix_quality'),
+                'statellites': parsed_data['GGA'].get('statellites'),
+                'altitude': parsed_data['GGA'].get('altitude')
+            })
+        elif sentence_type in ['GPRMC', 'BDRMC', 'GLRMC', 'GNRMC']:
+            parts = data.split(',')
+            timestamp, bj_time = conver_time_to_timestamp_and_bj(parts[1])
+            latitude = convert_to_decimal(parts[3]) if parts[4] == 'N' else -convert_to_decimal(parts[3])
+            longitude = convert_to_decimal(parts[5]) if parts[6] == 'E' else -convert_to_decimal(parts[5])
+
+            parsed_data['RMC'] = {
+                'time': parts[1],
+                'timestamp': timestamp,
+                'beijing_time': bj_time,
+                'status': parts[2],
+                'latitude': latitude,
+                'longitude': longitude,
+                'speed': parts[7],
+                'date': parts[9]
+            }
+            row.update({
+                'time': parsed_data['RMC'].get('time'),
+                'timestamp': parsed_data['RMC'].get('timestamp'),
+                'beijing_time': parsed_data['RMC'].get('beijing_time'),
+                'status': parsed_data['RMC'].get('status'),
+                'latitude': parsed_data['RMC'].get('latitude'),
+                'longitude': parsed_data['RMC'].get('longitude'),
+                'speed': parsed_data['RMC'].get('speed'),
+                'date': parsed_data['RMC'].get('date')
+            })
+        elif sentence_type in ['GPGSV', 'BDGSV', 'GLGSV']:
+            parts = data.split(',')
+            statellite_info = []
+            num_of_statellites = int(parts[3])
+            for i in range(num_of_statellites):
+                if 4 * i + 4 < len(parts):
+                    statellite_info.append({
+                        'statellite_prn': parts[4 + 4 * i],
+                        'elevation': parts[5 + 4 * i],
+                        'azimuth': parts[6 + 4 * i],
+                        'snr': parts[7 + 4 * i]
+                    })
+                    parsed_data['GSV'] = {'statellites_in_view': statellite_info}
+                    row[f'statellites_prn_{i + 1}'] = parts[4 + 4 * i]
+                    row[f'elevation_{i + 1}'] = parts[5 + 4 * i]
+                    row[f'azimuth_{i + 1}'] = parts[6 + 4 * i]
+                    row[f'snr_{i + 1}'] = parts[7 + 4 * i]
+        elif sentence_type in ['GPGNS', 'BDGNS', 'GLGNS']:
+            parts = data.split(',')
+            timestamp, bj_time = conver_time_to_timestamp_and_bj(parts[1])
+            latitude = convert_to_decimal(parts[2]) if parts[3] == 'N' else -convert_to_decimal(parts[2])
+            longitude = convert_to_decimal(parts[4]) if parts[5] == 'E' else -convert_to_decimal(parts[4])
+
+            parsed_data['GNS'] = {
+                'time': parts[1],
+                'timestamp': timestamp,
+                'beijing_time': bj_time,
+                'latitude': latitude,
+                'longitude': longitude,
+                'fix_quality': parts[6],
+                'statellites': parts[7],
+                'altitude': parts[9]
+            }
+            row.update({
+                'time': parsed_data['GNS'].get('time'),
+                'timestamp': parsed_data['GNS'].get('timestamp'),
+                'beijing_time': parsed_data['GNS'].get('beijing_time'),
+                'latitude': parsed_data['GNS'].get('latitude'),
+                'longitude': parsed_data['GNS'].get('longitude'),
+                'fix_quality': parsed_data['GNS'].get('fix_quality'),
+                'statellites': parsed_data['GNS'].get('statellites'),
+                'altitude': parsed_data['GNS'].get('altitude')
+            })
+
+        parsed_results.append(parsed_data)
+        table_data.append(row)
+
+    df = pd.DataFrame(table_data)
+    df.to_excel('nmeaParse_single.xlsx', index=False)
+
+    count = 1
+    with open('nmeaParse_single.txt', 'w') as outfile:
+        outfile.write('Longitude,Latitude,Elevation,name\n')
+        for pr in parsed_results:
+            if pr.get('RMC'):
+                outfile.write(f"{pr['RMC']['longitude']},{pr['RMC']['latitude']},{0.0},m{count}\n")
+                count += 1
+
+    return parsed_results
